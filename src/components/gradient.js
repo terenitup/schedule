edcffc5e269be40dc4a9a67c132310a7c4a35225
terenitup <terentaniuchi@gmail.com>
2018-09-17 11:50:09 -0600

    build gradient component

---
+++ src/components/gradient.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Gradient extends Component {
+
+  render() {
+    return (
+      <div className='gradient'>
+        
+      </div>
+    );
+  }
+}
+
+export default Gradient;
