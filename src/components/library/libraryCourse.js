95f3c3500fc3c791f94807e0e8d6db9b34b4de27
terenitup <terentaniuchi@gmail.com>
2018-09-20 11:13:54 -0600

    added logic to toggle enrolled reducer

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -17,7 +17,7 @@ class LibraryCourse extends Component {
         </div>
 
         <Arrow className='library-course__arrow' />
-        <Action onClick={this.props.toggleEnrolled(this.props.id)} className='library-course__action' />
+        <Action onClick={() => this.props.toggleEnrolled(this.props.id)} className='library-course__action' />
 
         <div className='library-course__description'>
             <label>Course Description</label>
